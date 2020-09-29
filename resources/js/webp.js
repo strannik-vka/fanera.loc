54dafeabeda7924a5a075d2e691611c8c538ba37
strannik-vka <canbka13@gmail.com>
2020-09-29 10:35:09 +0300

    цйу

--- resources/js/webp.js
+++ resources/js/webp.js
@@ -2,37 +2,62 @@ window.webp = {
 
     init: function () {
         if (!webp.support()) {
-            webp.to_png();
-            document.addEventListener('DOMSubtreeModified', function(){
+            webp.to_canvas();
+            document.addEventListener('DOMSubtreeModified', function () {
                 if (!webp.support()) {
-                    webp.to_png();
+                    webp.to_canvas();
                 }
             });
         }
     },
 
-    png: function (img) {
-        var canvas = document.createElement('canvas'),
-            context = canvas.getContext('2d');
-        canvas.width = img.width();
-        canvas.height = img.height();
-        context.drawImage(img[0], 0, 0);
-        return canvas.toDataURL('image/png');
+    dimensions(el) {
+        el = el[0];
+        if (el.naturalWidth != undefined) {
+            return {
+                'real_width': el.naturalWidth,
+                'real_height': el.naturalHeight,
+                'client_width': el.width,
+                'client_height': el.height
+            };
+        } else {
+            var img = new Image();
+            img.src = el.src;
+            var real_w = img.width;
+            var real_h = img.height;
+            return {
+                'real_width': real_w,
+                'real_height': real_h,
+                'client_width': el.width,
+                'client_height': el.height
+            };
+        }
     },
 
-    to_png: function () {
-        if($('img[src*=".webp"]:not(webp)').length){
+    canvas: function (img) {
+        var canvas = document.createElement('canvas'),
+            context = canvas.getContext('2d');
+
+        var dimensions = webp.dimensions(img);
+        canvas.width = dimensions.real_width;
+        canvas.height = dimensions.real_height;
+
+        context.drawImage(img[0], 0, 0);
+
+        img.before(canvas).hide();
+    },
+
+    to_canvas: function () {
+        if ($('img[src*=".webp"]:not(webp)').length) {
             $('img[src*=".webp"]:not(webp)').each(function (event) {
-                $(this).attr({
-                    'webp': 'true',
-                    'src': webp.png($(this))
-                });
+                $(this).attr('webp', true);
+                webp.canvas($(this));
             });
         }
     },
 
     support: function () {
-        if(typeof webp.support_status !== 'undefined'){
+        if (typeof webp.support_status !== 'undefined') {
             return webp.support_status;
         }
 
